18170ea356216b8ac3ffc794af8548e67d8393ac
Houston McClung <hmcclungiii@gmail.com>
2021-01-25 11:20:14 -0500

    Added media folder to settings.

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
